f6c08917c1b596d6b3a017ed642538eac09cd828
Xavier Bourguignon <xavierbourguignon@gmail.com>
2017-11-14 16:04:45 +0100

    Initial commit

---
+++ src/main/java/org/parisjug/NullPointerDereference.java
@@ -0,0 +1,19 @@
+package org.parisjug;
+
+public class NullPointerDereference {
+
+  public int searchForAnimalID(String animal) {
+    String trimmedAnimal = trim(animal);
+    if (trimmedAnimal.equals("Antonio")) {
+      return 1;
+    }
+    return -1;
+  }
+
+  private String trim(String animal) {
+    if(animal.isEmpty()) {
+      return null;
+    }
+    return animal.trim();
+  }
+}
---
+++ src/main/java/org/parisjug/QuizzUnreachableConditionalBranch.java
@@ -0,0 +1,16 @@
+package org.parisjug;
+
+public class QuizzUnreachableConditionalBranch {
+
+  public int ultimateBoringBusinessLogic(
+      int antonioAge, int nicolasAge, int zouheirAge) {
+    
+    if (antonioAge > nicolasAge 
+        && nicolasAge == zouheirAge 
+        && zouheirAge > antonioAge) {
+      return Integer.MAX_VALUE;
+    }
+    
+    return -1;
+  }  
+}
